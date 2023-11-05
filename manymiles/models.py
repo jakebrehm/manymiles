59ce64fac21bdfac38f874d90a81531f7d55274d
Jake Brehm <jake.m.brehm@gmail.com>
2023-11-05 16:35:09 -0500

    Added role and user_role models

--- manymiles/models.py
+++ manymiles/models.py
@@ -12,7 +12,11 @@ class User(Base):
 
     user_id = Column("id", Integer, primary_key=True)
     username = Column("username", String(32), nullable=False)
-    password_id = Column("password_id", ForeignKey("password.id", name="fk_user_password"), nullable=True)
+    password_id = Column(
+        "password_id",
+        ForeignKey("password.id", name="fk_user_password"),
+        nullable=True,
+    )
     password_hash = Column("password_hash", String(64), nullable=False)
     password_salt = Column("password_salt", LargeBinary, nullable=False)
     email = Column("email", String(319), nullable=False)
@@ -20,14 +24,8 @@ class User(Base):
     last_name = Column("last_name", String(35), nullable=True)
     created = Column("created", DateTime, nullable=True)
 
-    # def __init__(self, **kwargs) -> None:
-    #     """"""
-    #     super(User, self).__init__(**kwargs)
-
-    #     # self.password_id = 1000
-
     def __repr__(self) -> str:
-        return ("User("
+        return (f"{self.__class__.__name__}("
             f"user_id={repr(self.user_id)}, "
             f"username={repr(self.username)}, "
             f"password_id={repr(self.password_id)}, "
@@ -35,7 +33,7 @@ class User(Base):
             f"password_salt={repr(self.password_salt)}, "
             f"email={repr(self.email)}, "
             f"first_name={repr(self.first_name)}, "
-            f"last_name={repr(self.last_name)}"
+            f"last_name={repr(self.last_name)}, "
             f"created={repr(self.created)}"
         ")")
 
@@ -45,13 +43,17 @@ class Record(Base):
     __tablename__ = "record"
 
     record_id = Column("id", Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id", name="fk_record_user"))
+    user_id = Column(
+        Integer,
+        ForeignKey("user.id", name="fk_record_user"),
+        nullable=False,
+    )
     mileage = Column("mileage", Integer, nullable=False)
     recorded_datetime = Column("recorded_datetime", DateTime, nullable=False)
     notes = Column("notes", Text, nullable=True)
 
     def __repr__(self) -> str:
-        return ("Record("
+        return (f"{self.__class__.__name__}("
             f"record_id={repr(self.record_id)}, "
             f"user_id={repr(self.user_id)}, "
             f"mileage={repr(self.mileage)}, "
@@ -65,8 +67,19 @@ class Login(Base):
     __tablename__ = "login"
 
     login_id = Column("id", Integer, primary_key=True)
-    user_id = Column("user_id", ForeignKey("user.id", name="fk_login_user"))
+    user_id = Column(
+        "user_id",
+        ForeignKey("user.id", name="fk_login_user"),
+        nullable=False,
+    )
     login_datetime = Column("login_datetime", DateTime, nullable=False)
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+            f"login_id={repr(self.login_id)}, "
+            f"user_id={repr(self.user_id)}, "
+            f"login_datetime={repr(self.login_datetime)}"
+        ")")
 
 
 class Password(Base):
@@ -74,6 +87,56 @@ class Password(Base):
     __tablename__ = "password"
 
     password_id = Column("id", Integer, primary_key=True)
-    user_id = Column("user_id", ForeignKey("user.id", name="fk_password_user"), nullable=False)
+    user_id = Column(
+        "user_id",
+        ForeignKey("user.id", name="fk_password_user"),
+        nullable=False,
+    )
     password_hash = Column("password_hash", String(64), nullable=False)
     updated_datetime = Column("updated_datetime", DateTime, nullable=False)
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+            f"password_id={repr(self.password_id)}, "
+            f"user_id={repr(self.user_id)}, "
+            f"password_hash={repr(self.password_hash)}, "
+            f"updated_datetime={repr(self.updated_datetime)}"
+        ")")
+
+
+class Role(Base):
+
+    __tablename__ = "role"
+
+    role_id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+            f"role_id={repr(self.role_id)}, "
+            f"name={repr(self.name)}"
+        ")")
+
+
+class UserRole(Base):
+
+    __tablename__ = "user_role"
+
+    user_id = Column(
+        "user_id",
+        ForeignKey("user.id", name="fk_user_role_user"),
+        primary_key=True,
+        nullable=False,
+    )
+    role_id = Column(
+        "role_id",
+        ForeignKey("role.id", name="fk_user_role_role"),
+        primary_key=True,
+        nullable=False,
+    )
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+            f"user_id={repr(self.user_id)}, "
+            f"role_id={repr(self.role_id)}"
+        ")")
