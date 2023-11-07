26bf812157162d90b700c9fffe1589506718b7fe
Jake Brehm <jake.m.brehm@gmail.com>
2023-11-07 18:28:54 -0500

    Fully implemented login, logout, register functionality

--- manymiles/blueprints/login/login.py
+++ manymiles/blueprints/login/login.py
@@ -1,10 +1,15 @@
+import datetime as dt
+
 from flask import (
     Blueprint, flash, render_template, redirect, request, Response, session
 )
 
 from ...extensions import db
 from ...models import User
-from ... import utilities
+from ...utilities import (
+    create_account, generate_hash, is_correct_password, is_username_available,
+    is_valid_email, is_valid_password, log_login,
+)
 
 
 blueprint_login = Blueprint(
@@ -51,17 +56,23 @@ def validate_login() -> Response:
     # Get the user object
     user = db.session.query(User).filter_by(username=username).first()
 
-    # TODO: validate inputs
+    # Check if any of the required values are blank
+    if any(value is None for value in [username, password]):
+        flash("Please provide values for all required inputs.")
+        return redirect("/login")
 
-    # If the user was not found, flash an alert
-    if user is None:
+    # Check that the user exists
+    if not user:
         flash("An account with that username doesn't exist.")
         return redirect("/login")
 
-    # If the username/password combination is incorrect, flash an alert
-    if not utilities.is_correct_password(user, password):
+    # Check if the username/password combination is correct
+    if not is_correct_password(user, password):
         flash("The password entered does not match the one we have on file.")
         return redirect("/login")
+    
+    # Add login to the history table
+    log_login(user)
 
     # Store the user's details in the session
     session["user_id"] = user.user_id
@@ -89,17 +100,62 @@ def add_user():
     password = request.form.get("password")
     confirm_password = request.form.get("confirm-password")
 
-    # Confirm that the username is valid
+    # Get a list of required keys
+    optional_keys = ["first-name", "last-name"]
+    required_keys = request.form.to_dict()
+    for optional_key in optional_keys:
+        required_keys.pop(optional_key, None)
+
+    # Check if any of the required values are blank
+    if any(value is None for value in required_keys.values()):
+        flash("Please provide values for all required inputs.")
+        return redirect("/register")
+
+    # Confirm that the requested username is available
+    if not is_username_available(username):
+        flash("The requested username has already been taken.")
+        return redirect("/register")
+
+    # Confirm that the passwords are valid
+    if not (is_valid_password(password)):
+        # Don't need to check other password since it needs to be the same
+        flash(
+            "The password you entered is not valid. "
+            "The password must be at least 7 characters long and include "
+            "one lowercase letter, one uppercase letter, one number, and "
+            "one special character."
+        )
+        return redirect("/register")
 
     # Confirm that the passwords match
     if not (password == confirm_password):
-        ...
+        flash("The provided passwords do not match.")
+        return redirect("/register")
     
     # Confirm that the email is valid
-    if not utilities.is_valid_email(email):
+    if not is_valid_email(email):
         flash("The email provided is not valid.")
         return redirect("/register")
 
-    # TODO confirm that the passwords are valid
+    # Create the user's account with the requested details
+    hash, salt = generate_hash(password)
+    user = User(
+        username=username,
+        password_hash=hash,
+        password_salt=salt,
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+        created=dt.datetime.now(),
+    )
+    create_account(user)
 
-    return redirect("/register") # TODO
+    # Store the user's details in the session
+    session["user_id"] = user.user_id
+    session["username"] = user.username
+    session["first_name"] = user.first_name
+    session["last_name"] = user.last_name
+    session["email"] = user.email
+
+    # Redirect to the login screen on successful account creation
+    return redirect("/")
--- manymiles/utilities.py
+++ manymiles/utilities.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 from sqlalchemy.orm.session import Session
 
+from .extensions import db
 from . import models
 
 
@@ -43,7 +44,7 @@ def is_correct_password(user: models.User, password_to_verify: str) -> bool:
     return (user.password_hash == h.hexdigest())
 
 
-def log_current_password(session: Session, user: models.User) -> None:
+def log_current_password(user: models.User) -> None:
     """Adds the user's current password to the password history table.
     
     Also updates the `password_id` column of the User record.
@@ -57,36 +58,66 @@ def log_current_password(session: Session, user: models.User) -> None:
         password_hash=user.password_hash,
         updated_datetime=dt.datetime.now(),
     )
-    session.add(password)
-    session.commit()
+    db.session.add(password)
+    db.session.commit()
 
     # 
     user.password_id = password.password_id
-    session.commit()
+    db.session.commit()
 
 
-def create_account(session: Session, **kwargs) -> models.User:
+def log_login(user: models.User) -> None:
+    """Records the user's successful login in the login history table."""
+
+    # Add the login to the database
+    login = models.Login(
+        user_id=user.user_id,
+        login_datetime=dt.datetime.now(),
+    )
+    db.session.add(login)
+    db.session.commit()
+
+
+def create_account(user: models.User) -> models.User:
     """Creates an account and adds it to the database.
     
     Also adds a record to the password history table.
     """
 
     # Create a User object and add it to the database
-    user = models.User(**kwargs)
-    session.add(user)
-    session.commit()
+    db.session.add(user)
+    db.session.commit()
 
     # Log their password in the password history table
-    log_current_password(session, user)
+    log_current_password(user)
+
+    # Log the user's successful login to the database
+    log_login(user)
+
+
+def is_username_available(username: str) -> bool:
+    """Checks whether or not the provided username is available."""
+    
+    # See if there are any matches for the username in the database
+    matches = db.session.query(models.User.user_id).filter_by(username=username)
+    # If the username is available, there should have been no match
+    return matches.first() is None
 
 
 def is_valid_email(email: str) -> bool:
-    """Checks whether or not a provided email is valid."""
+    """Checks whether or not the provided email is valid."""
     
+    # TODO: Find better way
     expression = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
     return bool(re.match(expression, email))
 
 
 def is_valid_password(password: str) -> bool:
-    """Checks whether or not a provided password is valid."""
-    ...
+    """Checks whether or not the provided password is valid.
+    
+    The password must be at least 7 characters long and include one lowercase
+    letter, one uppercase letter, one number, and one special character.
+    """
+    
+    expression = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{7,}$"
+    return bool(re.match(expression, password))
