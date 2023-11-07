d531132782a14ee808a353b73c48e08eb1cd73b2
Jake Brehm <jake.m.brehm@gmail.com>
2023-11-07 10:30:16 -0500

    Got basic record pagination working

--- manymiles/blueprints/main/main.py
+++ manymiles/blueprints/main/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from flask import Blueprint, render_template, redirect, session
 
+from ...extensions import db
+from ...models import Record
+
 
 blueprint_main = Blueprint(
     "main",
@@ -18,16 +21,54 @@ def home() -> str:
         return redirect("/login")
     return render_template("main/home.html")
 
-@blueprint_main.route("/records")
-def records() -> str:
+@blueprint_main.route("/records", defaults={"page_num": 1, "per_page": 10})
+@blueprint_main.route("/records/<int:page_num>", defaults={"per_page": 10})
+@blueprint_main.route("/records/<int:page_num>/<int:per_page>")
+def records(page_num: int, per_page: int) -> str:
     """"""
-    return render_template("main/records.html")
+
+    # 
+    if not (user_id := session["user_id"]):
+        return redirect("/login")
+    
+    filtered_records = Record.query.filter_by(user_id=user_id)
+    ordered_records = filtered_records.order_by(Record.mileage.desc())
+    paginated_records = ordered_records.paginate(
+        per_page=per_page,
+        page=page_num,
+        error_out=True,
+    )
+
+    return render_template(
+        "main/records.html",
+        records=paginated_records,
+    )
 
 @blueprint_main.route("/_get_records", methods=["GET", "POST"])
 def _get_records() -> str:
-    df = pd.DataFrame({
-        "A": [1, 2, 3],
-        "B": [4, 5, 6],
-        "C": [7, 8, 9],
+    # df = pd.DataFrame({
+    #     "A": [1, 2, 3],
+    #     "B": [4, 5, 6],
+    #     "C": [7, 8, 9],
+    # })
+
+    # user_id = session["user_id"]
+    # if not user_id:
+    if not (user_id := session["user_id"]):
+        return redirect("/login")
+    
+    records = Record.query.filter_by(user_id=user_id).order_by(Record.mileage.desc()).paginate(per_page=10, page=1, error_out=True)
+    # print(pd.DataFrame.from_records(item.__dict__ for item in records.items).columns)
+    df = pd.DataFrame.from_records(item.__dict__ for item in records.items)
+    # print(df.head())
+    df["date"] = pd.to_datetime(df["recorded_datetime"]).dt.date.astype("string")
+    df["time"] = pd.to_datetime(df["recorded_datetime"]).dt.time.astype("string")
+    df = df[["record_id", "date", "time", "mileage", "notes"]]
+    df = df.rename(columns={
+        "record_id": "ID",
+        "date": "Date",
+        "time": "Time",
+        "mileage": "Mileage",
+        "notes": "Notes",
     })
     return df.to_json(orient="split", index=False)
--- manymiles/blueprints/main/static/js/render_table.js
+++ manymiles/blueprints/main/static/js/render_table.js
@@ -1,10 +1,6 @@
 function dataframeToHTML(json_string) {
     const dataframe = JSON.parse(json_string);
 
-    dataframe["columns"].forEach((element) => {
-        console.log(element)
-    });
-
     let html = "";
     
     html += `<table role="grid">`;
@@ -12,29 +8,45 @@ function dataframeToHTML(json_string) {
     dataframe["columns"].forEach((column) => {
         html += `<th>${column}</th>`;
     });
+    html += `<th></th>`;
+    // for (let [index, column] of dataframe["columns"].entries()) {
+    //     console.log(index);
+    //     if ([0, 1, 2, 3].includes(index)) {
+    //         html += `<th style="width: 15%">${column}</th>`;
+    //     } else {
+    //         html += `<th>${column}</th>`;
+    //     }
+    // }
     html += `</thead>`;
     html += `<tbody>`;
     dataframe["data"].forEach((row) => {
             html += `<tr>`;
             row.forEach((item) => {
+                if (row)
                 html += `<td>${item}</td>`;
             });
+            html += `<td>`;
+            html += `<a href="/delete_record?record=${row[0]}" role="button"><i class="fa fa-trash-o fa-fw"></i></a>`;
+            html += `<a href="/update?record=${row[0]}" role="button"><i class="fa fa-edit fa-fw"></i></a>`;
+            html += `</td>`;
             html += `</tr>`;
     });
     html += `</tbody>`;
     html += `</table>`;
 
-    console.log(html)
-
     return html;
 }
+
+// function()
 
 function renderRecordTable() {
     const button = document.getElementById("table-button");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState === XMLHttpRequest.DONE) {
-            document.getElementById("table").innerHTML = dataframeToHTML(xhttp.responseText);
+            document.getElementById("table").innerHTML = dataframeToHTML(
+                xhttp.responseText
+            );
             button.setAttribute("aria-busy", "false");
         }
     }
